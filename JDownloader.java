188723e26ba83be8c3e2d07863a3da5a33726318
Md. Mushiar Khan <mushiarkhan@gmail.com>
2016-12-26 11:21:43 +0000

    added gitignore file

--- JDownloader.java
+++ JDownloader.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.net.URLDecoder;
 import java.io.UnsupportedEncodingException;
 
